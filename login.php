262b723bcdc12f4c19c44e8b6d315f9485150198
Mykel Abania <mntabania@ceu.edu.ph>
2014-11-09 20:26:41 +0800

    *Login Update

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+echo"lalalalalal";
+if ($username == 'myk_bn' && $password == 'ewankosayo'){
+	echo "Welcome";
+}else{
+	echo "Invalid Login Information";
+}
+
+?>
