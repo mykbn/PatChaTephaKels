1f6a4070275f4a5cc395bddfb39665134e2c8024
Mykel Abania <mntabania@ceu.edu.ph>
2014-11-09 22:22:06 +0800

    *login database connection try

--- login.php
+++ login.php
@@ -1,12 +1,53 @@
 <?php
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-echo"lalalalalal";
-if ($username == 'myk_bn' && $password == 'ewankosayo'){
-	echo "Welcome";
-}else{
-	echo "Invalid Login Information";
+$inputUsername = $_POST['username'];
+$inputPassword = $_POST['password'];
+
+// $username = "root";
+// $password= "";
+// $database = "ceumoodle_database";
+// $serverName = "localhost";
+
+//Initialize Database Connection
+$conn = new mysqli("localhost", "root", "", "ceumoodle_database");
+
+//Check Connection
+if($conn->connect_error){
+	die("Connection Failed:" .$conn->connect_error);
 }
+echo "Connected Successfully";
+
+$queryUsername = "SELECT * FROM users WHERE 'Username' = '$inputUsername'";
+$resultUsername = mysqli_query($conn,$queryUsername)
+	or die("Error: ".mysqli_error($conn));
+$rowUser = mysqli_fetch_array($resultUsername);
+$serverUser = $rowUser["Username"];
+
+// $queryUsername = "SELECT * FROM 'users'";
+// $queryPassword = "SELECT * FROM 'users' WHERE 'Password' = '$inputPassword'";
+
+// $resultUsername = mysql_query($queryUsername);
+// $resultPassword = mysql_query($queryPassword);
+
+// echo $resultPasswordUsername;
+// $rowUser = mysql_fetch_array($resultUsername);
+// $rowPass = mysql_fetch_array($resultPassword);
+
+// $serverUser = $rowUser["Username"];
+// $serverPass = $rowPass["Password"];
+
+// echo $queryUsername;
+// echo $queryPassword;
+
+if($inputUsername == $serverUser){
+	echo "Successful!";
+}
+// mysql_close();
+
+// if ($username == 'myk_bn' && $password == 'ewankosayo'){
+// 	echo "Welcome";
+// }else{
+// 	echo "Invalid Login Information";
+// }
 
 ?>
